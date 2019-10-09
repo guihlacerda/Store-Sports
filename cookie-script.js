0eb483ec95c63f1671e7ac6da5fc6bb1634fbeca
Guilherme Lacerda <guilherme.lacerda@zoly.com.br>
2019-10-09 17:44:05 -0300

    Insert new function

--- cookie-script.js
+++ cookie-script.js
@@ -1,9 +1,11 @@
-let but = document.querySelector('button');
+(function() {
+    let but = document.querySelector('button');
 
-but.addEventListener('click', function(evt) {
-    evt.preventDefault;
-    let product = document.querySelector('.product');
-    let pid = product.getAttribute('data-product-id');
-    document.cookie = "cart=" + pid + "; expires=Wed, 11 Oct 2019 23:59:59 GMT;path=/";
-    window.location = '/Store-Sports/checkout.html';
-});  
+    but.addEventListener('click', function(evt) {
+        evt.preventDefault;
+        let product = document.querySelector('.product');
+        let pid = product.getAttribute('data-product-id');
+        document.cookie = "cart=" + pid + "; expires=Wed, 11 Oct 2019 23:59:59 GMT;path=/";
+        window.location = '/Store-Sports/checkout.html';
+    });   
+});

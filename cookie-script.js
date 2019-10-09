8c642522f9385f2b60ccb7e7bd3826f90c50faec
Guilherme Lacerda <guilherme.lacerda@zoly.com.br>
2019-10-09 18:06:54 -0300

    Change 124

--- cookie-script.js
+++ cookie-script.js
@@ -1,11 +1,12 @@
 (function() {
-    let but = document.querySelector('button');
-
+    var but = document.querySelector('button');
     but.addEventListener('click', function(evt) {
         evt.preventDefault;
-        let product = document.querySelector('.product');
-        let pid = product.getAttribute('data-product-id');
-        document.cookie = "cart=" + pid + "; expires=Wed, 11 Oct 2019 23:59:59 GMT;path=/";
-        window.location = 'https://guihlacerda.github.io/Store-Sports/checkout.html';
+        var product = document.querySelector('.product');
+        var pid = product.getAttribute('data-product-id');
+        document.cookie = "cart=" + pid + "; expires=Wed, 11 Oct 2019 23:59:59 GMT; path=/";
+        console.log(document.cookie);
+        // window.location = 'https://guihlacerda.github.io/Store-Sports/checkout.html';
+        return false;
     });   
 })();

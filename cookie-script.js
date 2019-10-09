85613a9fe19f0718ddf48992651c4a89de04c1f1
Guilherme Lacerda <guilherme.lacerda@zoly.com.br>
2019-10-09 15:35:51 -0300

    evento com cookie no botao de compra

--- cookie-script.js
+++ cookie-script.js
@@ -1,1 +1,11 @@
-document.cookie = "Procuct-one=true; expires=Wed, 11 Oct 2019 23:59:59 GMT";
+let but = document.querySelector('.desc a');
+
+but.addEventListener('click', function(evt) {
+    evt.preventDefault;
+    document.cookie = "Procuct-one=true; expires=Wed, 11 Oct 2019 23:59:59 GMT;path=/";
+    window.location = 'https://guihlacerda.github.io/Store-Sports/checkout.html';
+});
+
+
+
+

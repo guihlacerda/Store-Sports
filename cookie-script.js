e465b341ae62ef6130dd1e91ecb57bd6e14936c5
Guilherme Lacerda <guilherme.lacerda@zoly.com.br>
2019-10-09 16:35:57 -0300

    Change 4

--- cookie-script.js
+++ cookie-script.js
@@ -1,10 +1,14 @@
 (function() {
     let but = document.querySelector('button');
+    let location = 'https://guihlacerda.github.io/Store-Sports/checkout.html';
 
     but.addEventListener('click', function(evt) {
         evt.preventDefault;
         let product = document.querySelector('.product');
         let pid = product.getAttribute('data-product-id');
         document.cookie = "cart=" + pid + "; expires=Wed, 11 Oct 2019 23:59:59 GMT;path=/";
+        but = location;
     });
+
+    
 })();

dda5e218be06e3cc9dc5a2b4fa39fcdb9c6da373
Guilherme Lacerda <guilherme.lacerda@zoly.com.br>
2019-10-10 15:26:03 -0300

    Change 123413251

--- cookie-script.js
+++ cookie-script.js
@@ -1,13 +1,31 @@
 (function() {
-    var but = document.querySelector('button');
-    if (but !== null) {
-        but.addEventListener('click', function(evt) {
-            evt.preventDefault;
-            var product = document.querySelector('.product');
-            var pid = product.getAttribute('data-product-id');
-            document.cookie = "cart=" + pid + "; expires=Wed, 11 Oct 2019 23:59:59 GMT; path=/";
-            console.log(document.cookie);
-            window.location = 'https://guihlacerda.github.io/Store-Sports/checkout.html';
+    var body = document.querySelector('body');
+
+    // CHECKOUT page
+    if (body.classList.contains('checkout') == true) {
+        var cookies = document.cookie;
+        var strCookies = cookies.split(';');
+        var valueProduct = 'vazio';
+
+        strCookies.forEach(function(ck) {
+            if (ck.trim().slice(0, 5) == 'cart=') {
+                valueProduct = ck.split('=')[1]
+            };
         });
+        document.querySelector('.item[data-p="' + valueProduct + '"]').classList.add('visible');
+    }
+
+    // product page
+    if (body.classList.contains('product') == true) {
+        var but = document.querySelector('button');
+        if (but !== null) {
+            but.addEventListener('click', function(evt) {
+                evt.preventDefault;
+                var product = document.querySelector('.product');
+                var pid = product.getAttribute('data-product-id');
+                document.cookie = "cart=" + pid + "; expires=Wed, 11 Oct 2019 23:59:59 GMT; path=/";
+                window.location = 'https://guihlacerda.github.io/Store-Sports/checkout.html';
+            });
+        }
     }
 })();
